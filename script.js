3828bc1cd18e07148c0caca7af2c696844532f7e
ambroseotundo <ambroseotundo9@gmail.com>
2022-08-13 19:31:17 +0300

    project done

---
+++ script.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel')
+panels.forEach((panel) => {
+    // add event listener
+    panel.addEventListener('click', () =>{
+        // remove the active class from the other cards
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+
+    panels.forEach((panel) =>{
+        panel.classList.remove('active')
+    })
+}
